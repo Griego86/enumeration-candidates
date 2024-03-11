2d7f84d28527f301a108354178ef7852db9bee22
Griego86 <dimi1086@hotmail.com>
2024-03-10 18:40:35 -0700

    Implemented functions

--- filters.rb
+++ filters.rb
@@ -1,16 +1,47 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
+require './candidates'
+
 
 def find(id)
-    # Your code Here
-  end
+  @candidates.each{ |candidate| return candidate if candidate[:id] == id }
+  nil
+end
   
-  def experienced?(candidate)
-    # Your code Here
+
+def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
+end
+
+
+def qualified_candidates(candidates)
+  qualified = candidates.select do |candidate| 
+    has_required_experience(candidate) &&
+    has_enough_github_points(candidate) &&
+    knows_required_languages(candidate) &&
+    meets_age_requirement(candidate)
   end
+  qualified
+end
+
+
+def has_required_experience(candidate)
+  candidate[:years_of_experience] > 0
+end
+
+def has_enough_github_points(candidate)
+  candidate[:github_points] >= 100
+end
+
+def knows_required_languages(candidate)
+  candidate[:languages].include?("Ruby") || candidate[:languages].include?("Python")
+end
+
+def meets_age_requirement(candidate)
+  candidate[:age] > 17 
+end
+
   
-  def qualified_candidates(candidates)
-    # Your code Here
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
   end
-  
-  # More methods will go below
